5011ce3dd0c8b919e18684afa2e30cbe9b21afa3
XiNGRZ <chenxingyu92@gmail.com>
2019-12-27 12:08:04 +0800

    m1973: Add device specific system.prop
    Change-Id: I15a2f24887d7683be9660ab5a0ea927e521a5ed4

--- mokee_m1973.mk
+++ mokee_m1973.mk
@@ -40,3 +40,6 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="meizu_16sPro_CN-user 9 PKQ1.190616.001 1572446663 release-keys"
 
 BUILD_FINGERPRINT := meizu/meizu_16sPro_CN/meizu16sPro:9/PKQ1.190616.001/1572446663:user/release-keys
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.lcd_density=400
