e5c0c27994de119a3488fd935ce8a7201f9c07a5
XiNGRZ <chenxingyu92@gmail.com>
2020-01-03 13:33:27 +0800

    m1973: Set path of MX LED
    Change-Id: I7c27fcb751ff850c7202af23e55c2605b12227ee

--- device.mk
+++ device.mk
@@ -11,5 +11,9 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-mokee
 
+# Init
+PRODUCT_PACKAGES += \
+    init.target.rc
+
 # Inherit from sm8150-common
 $(call inherit-product, device/meizu/sm8150-common/common.mk)
