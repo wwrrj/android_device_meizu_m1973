975c74aa3b87cea1a627a0ddeff26c4fe2f44d4d
XiNGRZ <chenxingyu92@gmail.com>
2020-01-15 17:20:45 +0800

    m1973: Tunes FOD dimming range to reduce sudden brightness change
    Change-Id: I613fc25f665cc8ad9360e8f51c627d30b34d4dcd

--- mokee_m1973.mk
+++ mokee_m1973.mk
@@ -43,3 +43,7 @@ BUILD_FINGERPRINT := meizu/meizu_16sPro_CN/meizu16sPro:9/PKQ1.190616.001/1572446
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.sf.lcd_density=400
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    fod.dimming.min=40 \
+    fod.dimming.max=250
